ca528ec3f9396e95a515d5bd76f2baa6c7592272
Emmanuel Great <greatema03@gmail.com>
2026-02-05 14:48:49 +0100

    Update prayer.js

--- prayer.js
+++ prayer.js
@@ -37,3 +37,17 @@ window.addEventListener("scroll", function(){
         toTop.classList.remove("active");
     }
 })
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+      observer.unobserve(entry.target); // Play once
+    }
+  });
+}, { threshold: 0.25 }); // Trigger when 25% visible
+
+document.querySelectorAll(".reveal").forEach(el => {
+  observer.observe(el);
+});
+
