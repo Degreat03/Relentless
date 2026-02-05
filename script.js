e299011de2805ed18d1eae308435d1087e213289
Emmanuel Great <greatema03@gmail.com>
2026-02-05 13:32:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -451,6 +451,22 @@ window.addEventListener("scroll", function(){
     }
 })
 
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+      observer.unobserve(entry.target);
+    }
+  });
+}, {
+  threshold: 0.3
+});
+
+reveals.forEach(el => observer.observe(el));
+
+
 
 
 
